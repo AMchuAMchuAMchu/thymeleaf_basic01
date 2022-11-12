46b7175026bd72d597922277d03650c4e9d33e1c
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-12 15:35:39 +0800

    shino_hecate

--- src/main/java/org/kirito/controller/BasicSyntax.java
+++ src/main/java/org/kirito/controller/BasicSyntax.java
@@ -3,6 +3,7 @@ package org.kirito.controller;
 import org.junit.Test;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 /**
  * Description ==> TODO
@@ -12,8 +13,29 @@ import org.thymeleaf.context.Context;
  * CreateTime ==> 2022-11-12 14:52:52
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
-public class Demo01 {
+public class BasicSyntax {
 
+
+
+    @Test
+    public void testResolveHTML(){
+
+        TemplateEngine te = new TemplateEngine();
+
+        Context context = new Context();
+
+        context.setVariable("name","桐谷和人");
+
+        ClassLoaderTemplateResolver ctr = new ClassLoaderTemplateResolver();
+
+        te.setTemplateResolver(ctr);
+
+        String process = te.process("classpath:demo.html", context);
+
+        System.out.println("process==>"+process);
+
+
+    }
 
     @Test
     public void testInsertSyntax(){
@@ -22,7 +44,6 @@ public class Demo01 {
         context.setVariable("name","樱满集");
         String process = new TemplateEngine().process("<input type='text' th:value='${name}' />",context);
         System.out.println("process==>"+process);
-
 
     }
 

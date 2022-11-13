ca8d6a4c8c5af2bcb623898d493f709171e40d29
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-13 19:50:26 +0800

    shino_hecate

---
+++ basic_syntax_01/src/main/java/org/kirito/entity/Manga.java
@@ -0,0 +1,22 @@
+package org.kirito.entity;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> thymeleaf_basic01
+ * BelongsPackage ==> org.kirito.entity
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-13 19:45:36
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class Manga {
+
+    private String name;
+
+    private Integer time;
+
+    private String character01;
+
+}
--- basic_syntax_01/src/main/java/org/kirito/web/BasicSyntax.java
+++ basic_syntax_01/src/main/java/org/kirito/web/BasicSyntax.java
@@ -1,10 +1,15 @@
 package org.kirito.web;
 
+import org.kirito.entity.Manga;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Description ==> TODO
@@ -18,9 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class BasicSyntax {
 
+
+    @GetMapping("/object01")
+    public String object01(Model model){
+        Manga manga = new Manga();
+        manga.setName("罪恶王冠");
+        manga.setTime(2011);
+        manga.setCharacter01("楪祈");
+        model.addAttribute("manga",manga);
+        return "object01";
+    }
+
+
+    @GetMapping("variable01")
+    public String variable01(Model model){
+        model.addAttribute("name","黄金神威");
+        model.addAttribute("time","2018");
+        return "variable01";
+    }
+
     @GetMapping("/insert")
-    public String insert(Model model){
+    public String insert(Model model, HttpServletRequest httpServletRequest){
+        String name = httpServletRequest.getParameter("name");
         model.addAttribute("name","桐谷和人");
+        model.addAttribute("name",name);
         return "demo";
     }
 
